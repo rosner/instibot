264d37257562a580fe27afb4d7cf117587f0e8e7
Norman Rosner <normanrosner@googlemail.com>
2011-01-31 00:03:16 +0100

    Fixes the tests and extracts the creation of the tokenizer into the setUp method.

--- src/test/java/bitoflife/chatterbean/text/SubstitutionTest.java
+++ src/test/java/bitoflife/chatterbean/text/SubstitutionTest.java
@@ -1,5 +1,5 @@
 /*
-Copyleft (C) 2005 Hélio Perroni Filho
+Copyleft (C) 2005 Hï¿½lio Perroni Filho
 xperroni@yahoo.com
 ICQ: 2490863
 
@@ -10,163 +10,155 @@ ChatterBean is free software; you can redistribute it and/or modify it under the
 ChatterBean is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with ChatterBean (look at the Documents/ directory); if not, either write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA, or visit (http://www.gnu.org/licenses/gpl.txt).
-*/
+ */
 
 package bitoflife.chatterbean.text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import junit.framework.TestCase;
 
-public class SubstitutionTest extends TestCase
-{
-  /*
-  Attribute Section
-  */
+import org.junit.Before;
+import org.junit.Test;
 
-  private Substitution substitution;
+public class SubstitutionTest extends TestCase {
 
-  /*
-  Event Section
-  */
+    protected Substitution fixture;
 
-  protected void setUp()
-  {
-  }
+    protected Tokenizer tokenizer;
 
-  protected void tearDown()
-  {
-  }
-
-  /*
-  Test Section
-  */
-
-  public void testMultipleSubstitution() throws Exception
-  {
-    substitution = new Substitution("un do", "undo", TokenizerMother.newInstance());
-
-    List<String> expected = Arrays.asList(new String[] {"undo", "changes", "."});
-
-    List<String> actual =
-      new ArrayList<String>(
-        Arrays.asList(
-          new String[] {"un", "do", "changes", "."}));
-
-    substitution.substitute(actual);
-
-    assertEquals(expected, actual);
-  }
-
-  public void testOffsetSubstitution() throws Exception
-  {
-    substitution = new Substitution("for you", "for me", TokenizerMother.newInstance());
-
-    List<String> expected = Arrays.asList(new String[] {"It's", "for", "me", "not", "for", "you", "."});
-    List<String> original = Arrays.asList(new String[] {"It's", "for", "you", "not", "for", "you", "."});
-    List<String> actual = new ArrayList<String>(original);
-
-    int offset = substitution.substitute(0, actual);
-    assertEquals(0, offset);
-    assertEquals(original, actual);
-          
-    offset = substitution.substitute(1, actual);
-    assertEquals(3, offset);
-    assertEquals(expected, actual);
-  }
-  
-  public void testSinglePrefixSubstitution() throws Exception
-  {
-    substitution = new Substitution(" waht", " what", TokenizerMother.newInstance());
-
-    List<String> expected = Arrays.asList(new String[] {"whatever", "you", "say", "."});
-
-    List<String> actual =
-      new ArrayList<String>(
-        Arrays.asList(
-          new String[] {"Wahtever", "you", "say", "."}));
-
-    substitution.substitute(actual);
-
-    assertEquals(expected, actual);
-  }
-  
-  public void testSingleSufixSubstitution() throws Exception
-  {
-    substitution = new Substitution("'ll ", " will", TokenizerMother.newInstance());
-
-    List<String> expected = Arrays.asList(new String[] {"I", "will", "do", "the", "test", "."});
-
-    List<String> actual =
-      new ArrayList<String>(
-        Arrays.asList(
-          new String[] {"I'll", "do", "the", "test", "."}));
-        
-    substitution.substitute(actual);
-    
-    assertEquals(expected, actual);
-  }
-
-  public void testSingleWordSubstitution() throws Exception
-  {
-    substitution = new Substitution(" whatthe ", "what the", TokenizerMother.newInstance());
-
-    List<String> expected = Arrays.asList(new String[] {"Tell", "me", "what", "the", "problem", "is", "."});
-
-    List<String> actual =
-      new ArrayList<String>(
-        Arrays.asList(
-          new String[] {"Tell", "me", "whatthe", "problem", "is", "."}));
-
-    substitution.substitute(actual);
-
-    assertEquals(expected, actual);
-  }
-
-  public void testSuccessiveSubstitutions() throws Exception
-  {
-    List<String> original = Arrays.asList(new String[] {"I", "will", "do", "the", "test", "for", "him"});
-    List<String> expected = Arrays.asList(new String[] {"he", "or", "she", "will", "do", "the", "test", "for", "me"});
-    List<String> actual = new ArrayList<String>(original);
-
-    Tokenizer tokenizer = TokenizerMother.newInstance();
-    List<Substitution> substitutions =
-      Arrays.asList(
-        new Substitution[] {new Substitution(" I was ",  "he or she was", tokenizer),
-                            new Substitution(" he was ",  "I was", tokenizer),
-                            new Substitution(" she was ",  "I was", tokenizer),
-                            new Substitution(" I am ",  "he or she is", tokenizer),
-                            new Substitution(" I ",  "he or she", tokenizer),
-                            new Substitution(" he ",  "I", tokenizer),
-                            new Substitution(" she ",  "I", tokenizer),
-                            new Substitution(" me ",  "him or her", tokenizer),
-                            new Substitution(" him ",  "me", tokenizer),
-                            new Substitution(" her ",  "me", tokenizer),
-                            new Substitution(" my ",  "his or her", tokenizer),
-                            new Substitution(" myself ",  "him or herself", tokenizer),
-                            new Substitution(" mine ",  "his or hers", tokenizer)});
-
-    int i = 0, j = 0;
-    int[] offsets = {3, 9};
-    outer: for (; i < actual.size();)
-    {
-      int offset = i;
-      for (final Substitution substitution : substitutions)
-      {
-        i = substitution.substitute(offset, actual);
-        if (i > offset)
-        {
-          assertEquals(offsets[j++], i);
-          continue outer;
-        }
-        
-        i++;
-      }
+    @Before
+    protected void setUp() {
+	try {
+	    tokenizer = TokenizerMother.newInstance();
+	} catch (Exception e) {
+	    // this is probably not the 'correct' way
+	    throw new RuntimeException();
+	}
     }
 
-    assertEquals(9, i);
-    assertEquals(2, j); // Number of substitutions.
-    assertEquals(expected, actual);
-  }
+    protected void tearDown() {
+    }
+
+    @Test
+    public void testMultipleSubstitution() throws Exception {
+	fixture = new Substitution("un do", "undo", tokenizer);
+
+	List<String> expected = Arrays.asList(new String[] { "undo", "changes",
+		"." });
+
+	List<String> actual = new ArrayList<String>(Arrays.asList(new String[] {
+		"un", "do", "changes", "." }));
+
+	fixture.substitute(actual);
+
+	assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOffsetSubstitution() throws Exception {
+	fixture = new Substitution("for you", "for me", tokenizer);
+
+	List<String> expected = Arrays.asList(new String[] { "It's", "for",
+		"me", "not", "for", "you", "." });
+	List<String> original = Arrays.asList(new String[] { "It's", "for",
+		"you", "not", "for", "you", "." });
+	List<String> actual = new ArrayList<String>(original);
+
+	int offset = fixture.substitute(0, actual);
+	assertEquals(0, offset);
+	assertEquals(original, actual);
+
+	offset = fixture.substitute(1, actual);
+	assertEquals(3, offset);
+	assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSinglePrefixSubstitution() throws Exception {
+	fixture = new Substitution(" waht", " what", tokenizer);
+
+	List<String> expected = Arrays.asList(new String[] { "whatever", "you",
+		"say", "." });
+
+	List<String> actual = new ArrayList<String>(Arrays.asList(new String[] {
+		"Wahtever", "you", "say", "." }));
+
+	fixture.substitute(actual);
+
+	assertEquals(expected, actual);
+    }
+
+    public void testSingleSufixSubstitution() throws Exception {
+	fixture = new Substitution("'ll ", " will", tokenizer);
+
+	List<String> expected = Arrays.asList(new String[] { "I", "will", "do",
+		"the", "test", "." });
+
+	List<String> actual = new ArrayList<String>(Arrays.asList(new String[] {
+		"I'll", "do", "the", "test", "." }));
+
+	fixture.substitute(actual);
+
+	assertEquals(expected, actual);
+    }
+
+    public void testSingleWordSubstitution() throws Exception {
+	fixture = new Substitution(" whatthe ", "what the", tokenizer);
+
+	List<String> expected = Arrays.asList(new String[] { "Tell", "me",
+		"what", "the", "problem", "is", "." });
+
+	List<String> actual = new ArrayList<String>(Arrays.asList(new String[] {
+		"Tell", "me", "whatthe", "problem", "is", "." }));
+
+	fixture.substitute(actual);
+
+	assertEquals(expected, actual);
+    }
+
+    public void testSuccessiveSubstitutions() throws Exception {
+	List<String> original = Arrays.asList(new String[] { "I", "will", "do",
+		"the", "test", "for", "him" });
+	List<String> expected = Arrays.asList(new String[] { "he", "or", "she",
+		"will", "do", "the", "test", "for", "me" });
+	List<String> actual = new ArrayList<String>(original);
+
+	List<Substitution> substitutions = Arrays.asList(new Substitution[] {
+		new Substitution(" I was ", "he or she was", tokenizer),
+		new Substitution(" he was ", "I was", tokenizer),
+		new Substitution(" she was ", "I was", tokenizer),
+		new Substitution(" I am ", "he or she is", tokenizer),
+		new Substitution(" I ", "he or she", tokenizer),
+		new Substitution(" he ", "I", tokenizer),
+		new Substitution(" she ", "I", tokenizer),
+		new Substitution(" me ", "him or her", tokenizer),
+		new Substitution(" him ", "me", tokenizer),
+		new Substitution(" her ", "me", tokenizer),
+		new Substitution(" my ", "his or her", tokenizer),
+		new Substitution(" myself ", "him or herself", tokenizer),
+		new Substitution(" mine ", "his or hers", tokenizer) });
+
+	int i = 0, j = 0;
+	int[] offsets = { 3, 9 };
+	// FIXME: WTF?
+	outer: for (; i < actual.size();) {
+	    int offset = i;
+	    for (final Substitution substitution : substitutions) {
+		i = substitution.substitute(offset, actual);
+		if (i > offset) {
+		    assertEquals(offsets[j++], i);
+		    continue outer;
+		}
+
+		i++;
+	    }
+	}
+
+	assertEquals(9, i);
+	assertEquals(2, j); // Number of substitutions.
+	assertEquals(expected, actual);
+    }
 }
