a27857c4ca5ef1e424af5f39ee90f2a252c5cc57
Norman Rosner <normanrosner@googlemail.com>
2011-03-07 22:15:47 +0100

    Adds a simple method that renders the expiration date as string.

--- src/main/java/edu/potsdam/instibot/rest/UserCredentials.java
+++ src/main/java/edu/potsdam/instibot/rest/UserCredentials.java
@@ -1,18 +1,51 @@
 package edu.potsdam.instibot.rest;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import lombok.Data;
 
 import org.joda.time.DateTime;
 
-@Data @XmlRootElement(name="user_credentials")
+@XmlRootElement
 public class UserCredentials {
     
     protected String userId;
     
     protected String path;
     
-    protected DateTime expirationDate;    
+    protected DateTime expirationDate;
+
+    public UserCredentials() {
+	// TODO Auto-generated constructor stub
+    }
+
+    @XmlElement
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @XmlElement
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @XmlElement
+    public String getExpirationDateAsString() {
+        return expirationDate.toString();
+    }
     
+    public DateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(DateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }       
 }
